ba25dd5adfb995917a34d93c75e0c5ccb9f9b93b
John O. A. Rayner-Hilles <john.rayner-hilles@appexperts.me.uk>
2020-07-08 18:57:48 +0100

    Initial Structure #2

---
+++ app/src/main/java/io/github/occultus73/gitter/model/User.kt
@@ -0,0 +1,7 @@
+package io.github.occultus73.gitter.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class User(var whatever: String) : Parcelable
---
+++ app/src/main/java/io/github/occultus73/gitter/utils/Constants.kt
@@ -0,0 +1,4 @@
+package io.github.occultus73.gitter.utils
+
+class Constants {
+}
