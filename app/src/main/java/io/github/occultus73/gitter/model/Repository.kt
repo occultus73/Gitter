d76ab03bf675b9afc0ccac268e0d580369a7545d
John O. A. Rayner-Hilles <john.rayner-hilles@appexperts.me.uk>
2020-07-10 22:54:22 +0100

    Added Reso Coder's elegant thread-safe singleton for room database (however this may be redundant because of injection).

--- app/src/main/java/io/github/occultus73/gitter/model/Repository.kt
+++ app/src/main/java/io/github/occultus73/gitter/model/Repository.kt
@@ -8,7 +8,7 @@ import io.github.occultus73.gitter.model.network.RetrofitClient
 import io.github.occultus73.gitter.model.room.RepoDatabase
 
 class Repository(context: Context) {
-    private val roomRepoDatabase by lazy { Room.databaseBuilder(context, RepoDatabase::class.java, "repos.db").build() }
+    private val roomRepoDatabase by lazy { RepoDatabase(context) }
     private val roomRepoDAO by lazy { roomRepoDatabase.getRepoDAO() }
 
     val roomRepoData: LiveData<List<GithubRepository>> by lazy { roomRepoDAO.getAllRepos() }
