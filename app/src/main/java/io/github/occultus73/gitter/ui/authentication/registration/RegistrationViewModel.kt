ff5a5e99367eedf4a093e1930d4de21c6301c1c2
Debintom9633524015 <debintomdt@gmail.com>
2020-07-10 18:21:03 +0100

    Authentication started

--- app/src/main/java/io/github/occultus73/gitter/ui/authentication/registration/RegistrationViewModel.kt
+++ app/src/main/java/io/github/occultus73/gitter/ui/authentication/registration/RegistrationViewModel.kt
@@ -1,7 +1,77 @@
 package io.github.occultus73.gitter.ui.authentication.registration
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.occultus73.gitter.ui.authentication.AuthRepository
+import io.github.occultus73.gitter.utils.AuthListner
+import io.github.occultus73.gitter.utils.StateResponse
+import io.github.occultus73.gitter.utils.isSignUpDataValid
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class RegistrationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class RegistrationViewModel(private val authRepository: AuthRepository) : ViewModel() {
+
+    private val uiScope = CoroutineScope(Dispatchers.Main)
+    var email = MutableLiveData<String>()
+    var password = MutableLiveData<String>()
+    var confirmPassword = MutableLiveData<String>()
+    private var _errorCode = MutableLiveData<Int>()
+    lateinit var authListner : AuthListner
+    val errorCode  : LiveData<Int>
+        get() = _errorCode
+    private var _goToLogin = MutableLiveData<Boolean>()
+    val goToLogin : LiveData<Boolean>
+        get() = _goToLogin
+    init {
+        _goToLogin.postValue(false)
+    }
+
+
+
+    fun onSignUpClicked() {
+        var email = email.value.orEmpty()
+        var password = password.value.orEmpty()
+        var confirmPassword = confirmPassword.value.orEmpty()
+        var code = isSignUpDataValid(email, password, confirmPassword)
+        if(code == -1) {
+             uiScope.launch {
+                 signUpUser(email, password)
+             }
+        } else {
+            _errorCode.postValue(code)
+        }
+
+    }
+
+    fun goToLoginClicked() {
+        _goToLogin.postValue(true)
+    }
+
+    fun goToLoginComplete() {
+        _goToLogin.postValue(false)
+    }
+
+    private suspend fun signUpUser(email : String, password : String) {
+        authRepository.signUpUser(email, password)
+            .collect { state ->
+                when(state) {
+                    is StateResponse.Loading -> {
+                        authListner.onLoading()
+                    }
+                    is StateResponse.Success -> {
+                        state.data.user.sendEmailVerification()
+                        authListner.onSuccess()
+
+                    }
+                    is StateResponse.Failed -> {
+                        authListner.onFailure(state.message)
+                    }
+                }
+
+            }
+    }
+
 }
