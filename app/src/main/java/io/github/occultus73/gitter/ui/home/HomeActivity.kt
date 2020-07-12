15a8ea85701c795131bb6a4b3e57d20aa40b8510
John O. A. Rayner-Hilles <john.rayner-hilles@appexperts.me.uk>
2020-07-12 01:02:06 +0100

    added tysons HomeActivity.kt code

--- app/src/main/java/io/github/occultus73/gitter/ui/home/HomeActivity.kt
+++ app/src/main/java/io/github/occultus73/gitter/ui/home/HomeActivity.kt
@@ -3,11 +3,26 @@ package io.github.occultus73.gitter.ui.home
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelStoreOwner
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import io.github.occultus73.gitter.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : AppCompatActivity(), ViewModelStoreOwner {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.host_fragment)
+        bottomNav.setupWithNavController(navController)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
